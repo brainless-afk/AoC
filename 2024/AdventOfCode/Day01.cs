6119afae5e6a22b53f41c2341a957d7da65d94d8
brainless <46744361+brainless-afk@users.noreply.github.com>
2024-12-01 14:17:15 +0100

    2024 day 1

--- 2024/AdventOfCode/Day01.cs
+++ 2024/AdventOfCode/Day01.cs
@@ -2,14 +2,56 @@
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly string[] _lines;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _lines = [.. File.ReadAllText(InputFilePath).Split('\n')];
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    private (List<int>, List<int>) ParseInputToLists()
+    {
+        var leftIds = new List<int>();
+        var rightIds = new List<int>();
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        foreach (var line in _lines)
+        {
+            string[] numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            leftIds.Add(int.Parse(numbers[0]));
+            rightIds.Add(int.Parse(numbers[1]));
+        }
+
+        leftIds.Sort();
+        rightIds.Sort();
+
+        return (leftIds, rightIds);
+    }
+
+    private int CalculateTotalDistanceOfIds()
+    {
+        var (leftIds, rightIds) = ParseInputToLists();
+
+        int totalDistance = 0;
+        for (int i = 0; i < leftIds.Count; i++)
+        {
+            int distance = Math.Abs(leftIds[i] - rightIds[i]);
+            totalDistance += distance;
+        }
+
+        return totalDistance;
+    }
+
+    private int CalculateSimularityOfIds()
+    {
+        var (leftIds, rightIds) = ParseInputToLists();
+
+        var rightIdCounts = rightIds.CountBy(id => id).ToDictionary();
+
+        return leftIds.Sum(leftId => leftId * rightIdCounts.GetValueOrDefault(leftId));
+    }
+
+    public override ValueTask<string> Solve_1() => new(CalculateTotalDistanceOfIds().ToString());
+
+    public override ValueTask<string> Solve_2() => new(CalculateSimularityOfIds().ToString());
 }
